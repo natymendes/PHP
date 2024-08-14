1fa02e86936f60f418899ab22b636c6972edbe79
Elaine Natalia Mendes Menino <87189331+natymendes@users.noreply.github.com>
2024-08-14 09:44:47 -0300

    Create variaveis.php

---
+++ variaveis.php
@@ -0,0 +1,14 @@
+<?php
+// Declaração de variáveis
+$nome = "João";      // A variável $nome armazena uma string
+$idade = 30;         // A variável $idade armazena um inteiro
+$altura = 1.80;      // A variável $altura armazena um ponto flutuante (float)
+$ativo = true;       //A variável $ativo armazena um booleano (Verdadeiro ou falso)
+
+
+
+// Exibição dos valores das variáveis
+echo "Nome: $nome";  
+echo "Idade: $idade anos"; 
+echo "Altura: $altura metros"; 
+?>
