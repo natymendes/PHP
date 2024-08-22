3ebe0fb6d1cb593b30724fa5405752ba08ac6ad2
Elaine Natalia Mendes Menino <87189331+natymendes@users.noreply.github.com>
2024-08-21 21:53:10 -0300

    Create tipoInteiro.php

---
+++ tipoInteiro.php
@@ -0,0 +1,11 @@
+/*Em PHP, os tipos de dados são fundamentais para o funcionamento correto dos programas, pois determinam o tipo de valor que uma variável pode armazenar e 
+como esses valores são manipulados:
+
+1. Inteiros (int)
+Descrição: Números inteiros, sem casas decimais. */
+
+//Exemplo:
+
+<?php
+$idade = 25;
+?>
